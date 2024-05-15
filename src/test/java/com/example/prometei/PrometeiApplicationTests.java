13fc483b9edb8878030781fe132b5f58d14a57d3
AnnZhimol <ankavanka731@gmail.com>
2024-05-15 11:57:37 +0400

    init commit

---
+++ src/test/java/com/example/prometei/PrometeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prometei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
