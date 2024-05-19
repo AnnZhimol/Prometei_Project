a39a294732302b5732f382a744184cc8f4054b52
andrew <andrewstyou@gmail.com>
2024-05-20 01:06:03 +0400

    create base for dto

---
+++ src/main/java/com/example/prometei/dto/UserDto.java
@@ -0,0 +1,34 @@
+package com.example.prometei.dto;
+
+import com.example.prometei.models.UserGender;
+import com.example.prometei.models.UserRole;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Value;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * DTO for {@link com.example.prometei.models.User}
+ */
+@Value
+public class UserDto implements Serializable {
+    long id;
+    @Size(min = 5, max = 256)
+    @Pattern(regexp = "^(.+)@(.+)$")
+    String email;
+    @Size(min = 10)
+    String password;
+    UserGender gender;
+    String firstName;
+    String lastName;
+    @Size(min = 11)
+    String phoneNumber;
+    LocalDate birthDate;
+    String passport;
+    String residenceCity;
+    String internationalPassportNum;
+    LocalDate internationalPassportDate;
+    UserRole role;
+}
