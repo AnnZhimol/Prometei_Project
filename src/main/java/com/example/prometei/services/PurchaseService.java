ee5f9904cd25bea6256dc89caf6c5044a587e212
andrew <andrewstyou@gmail.com>
2024-05-19 23:05:39 +0400

    remove redundant else's in methods

--- src/main/java/com/example/prometei/services/PurchaseService.java
+++ src/main/java/com/example/prometei/services/PurchaseService.java
@@ -28,22 +28,20 @@ public class PurchaseService implements BasicService<Purchase> {
             purchaseRepository.save(entity);
             log.info("Purchase with id = {} successfully added", entity.getId());
         }
-        else {
-            log.error("Error adding purchase. Purchase = null");
-            throw new NullPointerException();
-        }
+
+        log.error("Error adding purchase. Purchase = null");
+        throw new NullPointerException();
     }
 
     @Override
     public void delete(Purchase entity) {
-        if (entity != null) {
-            purchaseRepository.delete(entity);
-            log.info("Purchase with id = {} successfully delete", entity.getId());
-        }
-        else {
+        if (entity == null) {
             log.error("Error deleting purchase. Purchase = null");
             throw new NullPointerException();
         }
+
+        purchaseRepository.delete(entity);
+        log.info("Purchase with id = {} successfully delete", entity.getId());
     }
 
     @Override
@@ -71,17 +69,16 @@ public class PurchaseService implements BasicService<Purchase> {
             log.error("Purchase with id = {} not found", id);
             throw new EntityNotFoundException();
         }
-        else {
-            currentPurchase = Purchase.builder()
-                    .id(currentPurchase.getId())
-                    .totalCost(entity.getTotalCost())
-                    .paymentMethod(entity.getPaymentMethod())
-                    .createDate(entity.getCreateDate())
-                    .build();
 
-            purchaseRepository.save(currentPurchase);
-            log.info("Purchase with id = {} successfully edit", id);
-        }
+        currentPurchase = Purchase.builder()
+                .id(currentPurchase.getId())
+                .totalCost(entity.getTotalCost())
+                .paymentMethod(entity.getPaymentMethod())
+                .createDate(entity.getCreateDate())
+                .build();
+
+        purchaseRepository.save(currentPurchase);
+        log.info("Purchase with id = {} successfully edit", id);
     }
 
     @Override
@@ -92,10 +89,9 @@ public class PurchaseService implements BasicService<Purchase> {
             log.info("Purchase with id = {} successfully find", id);
             return purchase;
         }
-        else {
-            log.error("Search purchase with id = {} failed", id);
-            return null;
-        }
+
+        log.error("Search purchase with id = {} failed", id);
+        return null;
     }
 
     public void addTicketsToPurchase(Purchase purchase, List<Ticket> tickets) {
@@ -103,31 +99,31 @@ public class PurchaseService implements BasicService<Purchase> {
             log.error("Adding tickets to the purchase failed. Purchase == null");
             throw new NullPointerException();
         }
-        else if (tickets == null) {
+
+        if (tickets == null) {
             log.error("Adding tickets to the purchase failed. Tickets == null");
             throw new NullPointerException();
         }
-        else {
-            purchase.setTickets(tickets);
 
-            Double cost = 0.0;
-            for (Ticket ticket : tickets) {
-                ticketService.addPurchaseToTicket(ticket, purchase);
+        purchase.setTickets(tickets);
 
-                if (ticket.getTicketType() == TicketType.BUSINESS)
-                    cost+=ticket.getFlight().getBusinessCost();
-                if (ticket.getTicketType() == TicketType.ECONOMIC)
-                    cost+=ticket.getFlight().getEconomyCost();
+        Double cost = 0.0;
+        for (Ticket ticket : tickets) {
+            ticketService.addPurchaseToTicket(ticket, purchase);
 
-                for (AdditionalFavor additionalFavor : ticket.getAdditionalFavors()) {
-                    cost += additionalFavor.getFlightFavor().getCost();
-                }
+            if (ticket.getTicketType() == TicketType.BUSINESS)
+                cost+=ticket.getFlight().getBusinessCost();
+            if (ticket.getTicketType() == TicketType.ECONOMIC)
+                cost+=ticket.getFlight().getEconomyCost();
+
+            for (AdditionalFavor additionalFavor : ticket.getAdditionalFavors()) {
+                cost += additionalFavor.getFlightFavor().getCost();
             }
-
-            purchase.setTotalCost(cost);
-            purchaseRepository.save(purchase);
-            log.info("Adding tickets to the purchase with id = {} was completed successfully", purchase.getId());
         }
+
+        purchase.setTotalCost(cost);
+        purchaseRepository.save(purchase);
+        log.info("Adding tickets to the purchase with id = {} was completed successfully", purchase.getId());
     }
 
     public void addUserToPurchase(Purchase purchase, User user) {
@@ -135,20 +131,20 @@ public class PurchaseService implements BasicService<Purchase> {
             log.error("Adding user to the purchase failed. Purchase == null");
             throw new NullPointerException();
         }
-        else if (user == null) {
+
+        if (user == null) {
             log.error("Adding user to the purchase failed. User == null");
             throw new NullPointerException();
         }
-        else {
-            purchase.setUser(user);
-            for(Ticket ticket : purchase.getTickets()) {
-                ticket.setUser(user);
-                ticketService.edit(ticket.getId(), ticket);
-            }
-            user.getPurchases().add(purchase);
-            userService.edit(user.getId(), user);
-            purchaseRepository.save(purchase);
-            log.info("Adding user to the purchase with id = {} was completed successfully", purchase.getId());
+
+        purchase.setUser(user);
+        for(Ticket ticket : purchase.getTickets()) {
+            ticket.setUser(user);
+            ticketService.edit(ticket.getId(), ticket);
         }
+        user.getPurchases().add(purchase);
+        userService.edit(user.getId(), user);
+        purchaseRepository.save(purchase);
+        log.info("Adding user to the purchase with id = {} was completed successfully", purchase.getId());
     }
 }
